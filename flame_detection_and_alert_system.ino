b0e0bc5dcd2408b8cb550374cc2f5fa227d5f01d
Saish Kadam <163175475+SaishKadam3112@users.noreply.github.com>
2025-12-06 11:02:13 +0530

    Define pin constants for flame sensor, buzzer, and servo

--- flame_detection_and_alert_system.ino
+++ flame_detection_and_alert_system.ino
@@ -1,3 +1,8 @@
 #include <Servo.h>
 
 Servo servoMotor;
+
+const int flamePin = 2;     // DO pin of flame sensor
+const int buzzerPin = 8;    // Buzzer pin
+const int servoPin = 9;     // Servo signal pin
+
